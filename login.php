2865a560bf3858cd2676ed321f6c9676673eea79
shashwat-garg <b16034@students.iitmandi.ac.in>
2017-11-24 19:28:11 +0530

    date format fixed

--- login.php
+++ login.php
@@ -32,12 +32,12 @@
 				$res = mysqli_query($con, "SELECT * FROM usersinfo WHERE email='$email'");
 				$row = mysqli_fetch_array($res, MYSQLI_ASSOC);
 				//execution time exceeding problem
-				//while(mysqli_num_rows($res) == 1){
+				if(mysqli_num_rows($res) == 1){
 				$_SESSION['username'] = $row['username'];
 				$_SESSION['email'] = $email;
 				$_SESSION['firstname'] = $row['firstname'];
 				$_SESSION['lastname'] = $row['lastname'];
-			//}
+				}
 				if($checkBox == "on")
 				{
 					setcookie("email",$email, time()+3600);
