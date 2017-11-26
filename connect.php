106233f9ca19fe310b58eabba5628282b2db6ab1
Dipayan Ghosh <33153253+dipayan6ghosh@users.noreply.github.com>
2017-11-26 21:50:38 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,6 +2,7 @@
 	
 	$con = mysqli_connect("localhost","root","","registration");
 	
+
 	if(mysqli_connect_errno())
 	{
 		echo "Error occured while connecting with database ".mysqli_connect_errno();
